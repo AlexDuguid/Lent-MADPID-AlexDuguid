650ab6f984d40a9e16f21bd557645e3468027692
AlexDuguid <77437959+AlexDuguid@users.noreply.github.com>
2021-01-14 10:35:34 +0000

    Sprite Creation + background and weapons

--- main.ts
+++ main.ts
@@ -1,1 +1,46 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . 4 4 . . . 
+        . . . . . . . 4 4 4 4 2 2 . . . 
+        . . . 4 4 4 4 4 4 2 2 2 4 . . . 
+        . . . . . . . . . . . 4 4 . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 200, 0)
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
+effects.starField.startScreenEffect()
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . 2 2 2 
+    . . . . . . . . . . . . 2 2 2 2 
+    . . . . . . . . . . 6 6 6 6 . . 
+    . . . . . . . . 6 6 6 9 9 6 . . 
+    . . . . . . 6 6 6 6 6 6 6 6 . . 
+    . . . . 6 6 6 6 8 8 8 6 9 9 . . 
+    . . . 6 6 6 6 6 8 8 8 6 9 9 . . 
+    . 6 6 6 6 6 6 6 6 6 6 6 9 9 . . 
+    6 6 6 6 6 8 8 8 6 6 6 6 9 9 . . 
+    6 6 6 6 6 8 8 8 6 6 6 6 9 9 . . 
+    . . . 6 6 6 6 6 6 8 8 8 9 9 . . 
+    . . . . . . 6 6 6 8 8 8 6 6 . . 
+    . . . . . . . . 6 6 6 6 6 6 . . 
+    . . . . . . . . . 6 6 6 2 2 2 2 
+    . . . . . . . . . . . . . 2 2 2 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.setFlag(SpriteFlag.StayInScreen, true)
+game.onUpdateInterval(500, function () {
+	
+})
